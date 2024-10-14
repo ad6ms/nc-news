e0bbc65ecc2e2379651283027b7c78ba4ffdeea7
ad6ms <StansfieldA96@gmail.com>
2024-10-14 12:24:58 +0100

    adding /api endpoint and relevant test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const app = require("../app");
 const data = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
 const request = require("supertest");
+const { response } = require("express");
 
 beforeEach(() => seed(data));
 
@@ -11,7 +12,7 @@ afterAll(() => {
 });
 
 describe("Testing endpoints in the app.js file", () => {
-  test("GET: 200 - request to api/topcs returns all available topics", () => {
+  test("GET: 200 - request to /api/topcs returns all available topics", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
@@ -25,6 +26,15 @@ describe("Testing endpoints in the app.js file", () => {
             })
           );
         });
+      });
+  });
+  test("GET: 200 - request to /api returns a list of all available endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        const endpoints = response.body.endpoints;
+        expect(typeof endpoints).toBe("object");
       });
   });
 });
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const getTopics = require("./controllers/topic-controller");
+const endpoints = require("./endpoints.json");
+
+app.get("/api", (request, response) => {
+  return response.status(200).send({ endpoints });
+});
 
 app.get("/api/topics", getTopics);
 
