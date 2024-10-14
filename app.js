95cab64689ab84c9272cb164baaefd85ded6144d
ad6ms <StansfieldA96@gmail.com>
2024-10-14 16:03:54 +0100

    adding endpoint for getting all articles

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const endpoints = require("./endpoints.json");
 const getTopics = require("./controllers/topic-controller");
-const getArticleById = require("./controllers/article-controller");
+const {
+  getArticleById,
+  getAllArticles,
+} = require("./controllers/article-controller");
 
 app.get("/api", (request, response) => {
   return response.status(200).send({ endpoints });
@@ -11,5 +14,7 @@ app.get("/api", (request, response) => {
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getAllArticles);
 
 module.exports = app;
